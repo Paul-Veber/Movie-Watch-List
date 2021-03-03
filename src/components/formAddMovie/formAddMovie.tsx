12b59a2bae34c2108b5436cae37d52e4f0a6276e
FalconPilot333 <fpilot.github@gmail.com>
2021-03-03 17:04:31 +0100

    Add demo of factorisation and props typing

--- src/components/formAddMovie/formAddMovie.tsx
+++ src/components/formAddMovie/formAddMovie.tsx
@@ -1,46 +1,91 @@
 import {
+  ChangeEvent,
+  Dispatch,
+  FormEvent,
   FunctionComponent,
+  SetStateAction,
 } from "react"
+import { Movie } from "../../types/types"
 
-export const FormAddMovie: FunctionComponent = (props: any) => {
+interface InputProps {
+  title: string
+  tag: keyof Movie
+  onChange: <V extends ValueOf<Movie>>(key: keyof Movie, parseFunc: (v: string) => V) => (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+const TextInput: React.FunctionComponent<InputProps> = ({
+  title,
+  tag,
+  onChange
+}) => (
+  <label htmlFor={tag} className="form__label">
+    {title}
+    <input
+      type="text"
+      name={tag}
+      className="form__input"
+      onChange={onChange<string>(tag, String)}
+    />
+  </label>
+)
+
+const NumberInput: React.FunctionComponent<InputProps> = ({
+  title,
+  tag,
+  onChange
+}) => (
+  <label htmlFor={tag} className="form__label">
+    {title}
+    <input
+      type="number"
+      name={tag}
+      className="form__input"
+      onChange={onChange<number>(tag, Number)}
+    />
+  </label>
+)
+
+export interface FormAddMovieProps {
+  propsAddMovieTolist: (event: FormEvent<HTMLFormElement>) => void
+  setMovie: Dispatch<SetStateAction<Movie>>
+  movie: Movie
+}
+
+type ValueOf<T> = T[keyof T]
+
+const patchMovie = (
+  setMovie: FormAddMovieProps['setMovie'],
+  movie: Movie
+) => <ExpectedValue extends ValueOf<Movie>>(
+  key: keyof Movie,
+  parseFunc: (v: string) => ExpectedValue
+) => (event: ChangeEvent<HTMLInputElement>): void => {
+  setMovie({ ...movie, [key]: parseFunc(event.target.value) })
+}
+
+export const FormAddMovie: FunctionComponent<FormAddMovieProps> = ({
+  propsAddMovieTolist,
+  setMovie,
+  movie
+}) => {
+  const updateMovie = patchMovie(setMovie, movie)
   return (
-    <form action="" key={0} onSubmit={props.propsAddMovieTolist}>
-      <label htmlFor="name" className="form__label">
-        Nom
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="form__input"
-          onChange={(e) =>
-            props.setMovie({ ...props.movie, name: e.target.value })
-          }
-        />
-      </label>
-      <label htmlFor="type" className="form__label">
-        Genre
-        <input
-          type="text"
-          name="type"
-          id="type"
-          className="form__input"
-          onChange={(e) =>
-            props.setMovie({ ...props.movie, type: e.target.value })
-          }
-        />
-      </label>
-      <label htmlFor="real" className="form__label">
-        Realisateur
-        <input
-          type="text"
-          name="real"
-          id="real"
-          className="form__input"
-          onChange={(e) =>
-            props.setMovie({ ...props.movie, realisator: e.target.value })
-          }
-        />
-      </label>
+    <form action="" key={0} onSubmit={propsAddMovieTolist}>
+      <TextInput
+        title="Nom"
+        tag="name"
+        onChange={updateMovie}
+      />
+      <TextInput
+        title="Genre"
+        tag="type"
+        onChange={updateMovie}
+      />
+      <TextInput
+        title="Réalisateur"
+        tag="realisator"
+        onChange={updateMovie}
+      />
 
       <div className="form__duration">
         <label htmlFor="hourDuration" className="form__label">
